8307d1035c33d97a3015599f0d23f81626b72ca5
Alexander Gansky <alexander.gansky@spryker.com>
2017-09-12 16:31:13 +0000

    1:
    - fixed formatting
    - added minuses

--- index.php
+++ index.php
@@ -7,15 +7,13 @@ $meat = array('duck', 'rabbit', 'pork');
 $food = array ($fruits, $vegetables, $meat);
 
 // обход двумерного массива
-for($i=0; $i < count($food); $i++)
+for($i = 0; $i < count($food); $i++)
 {
-
-
-for($q=0; $q < count($food[$i]); $q++)
-{
-echo  " | ".$food[$i][$q];
+    for($q = 0; $q < count($food[$i]); $q++)
+    {
+        echo ' | ' . $food[$i][$q];
+        echo '---------------------------';
+    }
+    echo "\n";
 }
-echo  "\n";
-}
 
-?>
