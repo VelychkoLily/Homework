1dbb234d077ed48271c82b82c212bcd0c1388800
Lily <velychko.lily@gmail.com>
2017-09-15 20:38:00 +0300

    wagon raboty

--- function.php
+++ function.php
@@ -1,25 +1,22 @@
 <?php
-$fruits = array ('apple', 'pear', 'nectarine');
-$vegetables = array ('carrot', 'cucumber', 'onion');
-$meat = array('duck', 'rabbit', 'pork');
 
-$food = array ($fruits, $vegetables, $meat);
-
-function writeArrayInFile($foodArray){
-$serArray = serialize($foodArray); // преобразовываем массив в строку
-$file = fopen ("array.txt","w+"); // открываем файл, если надо то создаем
-fputs($file, $serArray);// записываем в него строку
-fclose($file); // закрываем файл
-}
-
-function readArrayInFile($fileName)
+function readArrayInFile($filename)
 {
-    $file = fopen($fileName, 'r'); // открываем файл
-    $str = "";
-    // считываем все из файла
-    while (($buffer = fgets($file, 128)) !== false) {
-        $str .= $buffer;
-    }
-    $array = unserialize($str); // преобразовываем строку в массив
-    return $array;
+    $text = file_get_contents($filename);
+    $foodArray = unserialize($text);//восстановление
+    return $foodArray;
 }
+
+function printText($arrayResult)
+{
+
+    for ($i = 0; $i < count($arrayResult); $i++) {
+        for ($q = 0; $q < count($arrayResult[$i]); $q++) {
+            echo " | " . $arrayResult[$i][$q];
+        }
+        echo "\n";
+    }
+
+}
+
+
--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
-$filename = 'array.txt';
+
 require_once('function.php');
-writeArrayInFile($food);
-$arrayResult = readArrayInFile('function.php');
+require_once('config.php');
 
+$arrayResult = readArrayInFile(PATH);
 
-for ($i = 0; $i < count($arrayResult); $i++) {
-
-    for ($q = 0; $q < count($arrayResult[$i]); $q++) {
-        echo " | " . $arrayResult[$i][$q];
-    }
-    echo "\n";
-
-}
+printText( $arrayResult);
